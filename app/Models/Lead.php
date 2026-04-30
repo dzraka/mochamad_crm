a87246d55f7ff51d81e38949d02d699ba7d23f1f
Mochamad Dzaki Raka Maulana <muhdzakirakamaulana10@gmail.com>
2026-04-30 18:03:29 +0700

    fix: update fillable properties in Lead model

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -11,7 +11,7 @@ class Lead extends Model
     protected $fillable = [
         'user_id',
         'name',
-        'phone',
+        'contact',
         'address',
         'notes',
         'status'
