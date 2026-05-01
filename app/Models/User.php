03f11e4d84cbd4344a047d4167903b2fed358d3c
Mochamad Dzaki Raka Maulana <muhdzakirakamaulana10@gmail.com>
2026-05-01 21:41:32 +0700

    fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,10 +10,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 #[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -54,5 +56,10 @@ class User extends Authenticatable
     public function isSales(): bool
     {
         return $this->role === 'sales';
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
